79c2d54e303d867feb29a73c58b04d51df0f49f2
FrankTII <frank.training2@gmail.com>
2023-04-29 21:45:31 -0600

    Commenting code for Populator

--- Sesion-07/Reto3/src/main/java/com/example/ejemplo1/Ejemplo1Application.java
+++ Sesion-07/Reto3/src/main/java/com/example/ejemplo1/Ejemplo1Application.java
@@ -4,6 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+/*
+import org.springframework.beans.factory.annotation.Autowired;
+*/
+/*
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+*/
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+*/
+/*
+import org.springframework.transaction.PlatformTransactionManager;
+*/
 @EnableWebFlux
 @SpringBootApplication
 public class Ejemplo1Application {
@@ -11,5 +28,12 @@ public class Ejemplo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Ejemplo1Application.class, args);
 	}
-
+	/*
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+		Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+		factory.setResources(new Resource[]{new ClassPathResource("EmpleadosCargados.json")});
+		return factory;
+	}
+	*/
 }
