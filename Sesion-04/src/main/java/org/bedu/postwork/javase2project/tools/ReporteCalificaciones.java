14663e59687735ad5dc790742e553101c0e62c72
FrankTII <frank.training2@gmail.com>
2023-05-02 00:05:24 -0600

    Update al postworks incrementally
    3->4->5->6

---
+++ Sesion-04/src/main/java/org/bedu/postwork/javase2project/tools/ReporteCalificaciones.java
@@ -0,0 +1,61 @@
+package org.bedu.postwork.javase2project.tools;
+
+import org.bedu.postwork.javase2project.model.Curso;
+import org.bedu.postwork.javase2project.model.Estudiante;
+import org.bedu.postwork.javase2project.model.Materia;
+import org.bedu.postwork.javase2project.service.CreadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Component
+public class ReporteCalificaciones {
+    @Autowired
+    private CreadorService creadorService;
+
+    public List<String> listaMaterias(){
+        Iterable<Materia> materias = creadorService.getMateria();
+
+       List<String> listaMateria = StreamSupport.stream(materias.spliterator(), false)
+                .map(Materia::getNombre)
+                .sorted()
+                .collect(Collectors.toList());
+
+       return listaMateria;
+
+        //listaMateria.forEach(System.out::println);
+    }
+
+    public Map<String, Integer> listaAlumnosPorCalificacion(Curso curso){
+            Map<String, Integer> valores = new LinkedHashMap<>();
+            curso.getCalificaciones().entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .forEach(entry -> {
+                valores.put(entry.getKey().getNombreCompleto(), entry.getValue());
+                    });
+
+
+        return valores;
+
+
+    }
+
+    public Map<String, Integer> listaAlumnosPorEstudiantes(Curso curso){
+
+        Map<String, Integer> valores = new TreeMap<>();
+        curso.getCalificaciones().entrySet().stream()
+                .forEach(entry -> {
+                    valores.put(entry.getKey().getNombreCompleto(), entry.getValue());
+                });
+
+        return valores;
+    }
+
+
+}
