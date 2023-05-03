89901c73207907baab87de8b9422bea9638cd8a2
FrankTII <frank.training2@gmail.com>
2023-05-02 21:46:27 -0600

    Sesion8:Updaload sesion6 into sesion8 folder code to start refactoring

--- Sesion-08/src/test/java/org/bedu/postwork/javase2project/Javase2ProjectApplicationTests.java
+++ Sesion-08/src/test/java/org/bedu/postwork/javase2project/Javase2ProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ejemplo1;
+package org.bedu.postwork.javase2project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ejemplo1ApplicationTests {
+class Javase2ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
